3b06a6afe49edde736d29a8eba723e9cc292e140
Andryu <shunsuke.andoh@gmail.com>
2014-10-06 19:48:49 -0400

    git ブランチ名をプロンプトで表示

--- setup.sh
+++ setup.sh
@@ -5,3 +5,9 @@ mkdir  ~/.vim/compiler
 mkdir  ~/.vim/snippets
 git clone git://github.com/Shougo/neobundle.vim  ~/.vim/bundle/neobundle.vim
 cp .vimrc ~/
+
+# git ブランチ名補完
+wget https://github.com/git/git/raw/master/contrib/completion/git-completion.bash ~/.git-completion.bash
+
+# プロンプトにブランチ表示
+wget https://raw.github.com/git/git/master/contrib/completion/git-prompt.sh ~/.git-prompt.sh
