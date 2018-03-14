792b0718c68c842a30f59b7f47991e3a9de903a0
安藤駿介 <ando.shunsuke@adways.net>
2018-03-14 02:26:40 +0000

    [lib] Add dein

--- .vimrc
+++ .vimrc
@@ -1,11 +1,11 @@
 " Neobundle 設定読み込み
-if filereadable(expand('$HOME/dotfiles/.vim/neobundle.vim'))
-    source $HOME/dotfiles/.vim/neobundle.vim
-endif
-
-" 分割した設定ファイルをすべて読み込む
-set rtp+=$HOME/dotfiles/.vim
-runtime! conf.d/*.vim
+"if filereadable(expand('$HOME/dotfiles/.vim/neobundle.vim'))
+"    source $HOME/dotfiles/.vim/neobundle.vim
+"endif
+"
+"" 分割した設定ファイルをすべて読み込む
+"set rtp+=$HOME/dotfiles/.vim
+"runtime! conf.d/*.vim
 
 
 " ここからdein
--- setup.sh
+++ setup.sh
@@ -1,13 +1,18 @@
 #!/bin/sh
 #yum -y install vim-enhanced
-mkdir -p ~/.vim/bundle
-mkdir  ~/.vim/compiler
-mkdir  ~/.vim/snippets
-git clone https://github.com/Shougo/neobundle.vim  ~/.vim/bundle/neobundle.vim
-cp .vimrc ~/
+#mkdir -p ~/.vim/bundle
+#mkdir  ~/.vim/compiler
+#mkdir  ~/.vim/snippets
+#git clone https://github.com/Shougo/neobundle.vim  ~/.vim/bundle/neobundle.vim
+#cp .vimrc ~/
+#
+## git ブランチ名補完
+#wget https://github.com/git/git/raw/master/contrib/completion/git-completion.bash ~/.git-completion.bash
+#
+## プロンプトにブランチ表示
+#wget https://raw.github.com/git/git/master/contrib/completion/git-prompt.sh ~/.git-prompt.sh
 
-# git ブランチ名補完
-wget https://github.com/git/git/raw/master/contrib/completion/git-completion.bash ~/.git-completion.bash
-
-# プロンプトにブランチ表示
-wget https://raw.github.com/git/git/master/contrib/completion/git-prompt.sh ~/.git-prompt.sh
+# TODO 置き場所は後で
+# deinを持ってくる
+mkdir -p ~/.vim/dein/repos/github.com/Shougo/dein.vim
+git clone https://github.com/Shougo/dein.vim.git  ~/.vim/dein/repos/github.com/Shougo/dein.vim
