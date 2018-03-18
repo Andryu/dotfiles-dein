fb9969e96bd5b1e56d13f3932fbc87915a100c8b
Andryu <shunuske.andoh@gmail.com>
2018-03-18 14:50:41 +0000

    [feat] ディレクトリ名変更

--- alias.sh
+++ alias.sh
@@ -6,7 +6,7 @@ alias gb="git branch"
 alias gba="git branch -a"
 alias gf="git fetch"
 
-source ~/dotfiles/git-completion.bash
-source ~/dotfiles/git-prompt.sh
+source ~/dotfiles-dein/git-completion.bash
+source ~/dotfiles-dein/git-prompt.sh
 GIT_PS1_SHOWDIRTYSTATE=true
 export PS1='\[\033[32m\]\u@\h\[\033[00m\]:\[\033[34m\]\w\[\033[31m\]$(__git_ps1)\[\033[00m\]\$ \ncmd >'
