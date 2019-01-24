6beccf7026c5e749e0e786416a7f2afad246dce7
Shunsuke Andoh <shunsuke.andoh@gmail.com>
2019-01-24 11:17:26 +0900

    git補完ファイルなどの出力先、ファイル名を変更

--- setup.sh
+++ setup.sh
@@ -8,11 +8,11 @@ mkdir  ~/.vim/compiler
 mkdir  ~/.vim/snippets
 cp .vimrc ~/
 
-# git ブランチ名補完
-wget https://github.com/git/git/raw/master/contrib/completion/git-completion.bash ~/.git-completion.bash
-
-# プロンプトにブランチ表示
-wget https://raw.github.com/git/git/master/contrib/completion/git-prompt.sh ~/.git-prompt.sh
+# git ブランチ名補完                                                                                                                                                                                            
+wget https://github.com/git/git/raw/master/contrib/completion/git-completion.bash -O ./.git-completion.bash                                                                                                     
+                                                                                                                                                                                                                
+# プロンプトにブランチ表示                                                                                                                                                                                      
+wget https://raw.github.com/git/git/master/contrib/completion/git-prompt.sh -O ./.git-prompt.sh   
 
 # TODO 置き場所は後で
 # deinを持ってくる
